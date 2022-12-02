0b3352d03f69b66003092a3f5fb2e31a54c06681
GuidoClas <gclas@santandertecnologia.com.ar>
2022-12-02 14:09:30 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+}
